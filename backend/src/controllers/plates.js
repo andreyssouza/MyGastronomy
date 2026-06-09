e495a6863d4a08e3d9444e3057ea50e169977e9b
Andreyssouza <seuemail@exemplo.com>
2026-06-09 16:04:50 -0300

    CRUD completo com nossos pratos

---
+++ backend/src/controllers/plates.js
@@ -0,0 +1,58 @@
+import PlatesDataAcess from "../dataAccess/plates.js";
+import { ok, serverError } from "../helpers/httpResponse.js";
+
+export default class PlatesControllers {
+  constructor() {
+    this.dataAccess = new PlatesDataAccess();
+  }
+
+  async getPlates() {
+    try {
+      const plates = await this.dataAccess.getPlates();
+      console.log(plates);
+      return ok(plates);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+
+  async getAvailablePlates() {
+    try {
+      const plates = await this.dataAccess.getAvailablePlates();
+
+      return ok(plates);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+
+  async addPlate(plateData) {
+    try {
+      const result = await this.dataAccess.addPlate(plateData);
+
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+
+  async deletePlate(platesId) {
+    try {
+      const result = await this.dataAccess.deletePlate(platesId);
+
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+
+  async updatePlate(plateId, plateData) {
+    try {
+      const result = await this.dataAccess.updatePlate(plateId, plateData);
+
+      return ok(result);
+    } catch (error) {
+      return serverError(error);
+    }
+  }
+}
