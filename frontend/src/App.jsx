9bf62c9db97c870ab94c086506588e70210edd85
Andrey Souza <andreyschowza@gmail.com>
2026-06-11 15:35:41 -0300

    Iniciando o frontend do zero

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <h1 style={{ background: "red", width: "fit-content", padding: "10px" }}>
+        {" "}
+        My GASTRONOMY
+      </h1>
+      <p>Fala galera do canal</p>
+    </>
+  );
+}
