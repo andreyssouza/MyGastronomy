ad8e540b595d965cb38a22e271b8e8b7f8e0b896
Andrey Souza <andreyschowza@gmail.com>
2026-06-12 00:16:48 -0300

    Criando componentes com react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
+import Navbar from "./components/navbar/navbar";
+
 export default function App() {
   return (
     <>
-      <h1 style={{ background: "red", width: "fit-content", padding: "10px" }}>
-        {" "}
-        My GASTRONOMY
-      </h1>
-      <p>Fala galera do canal</p>
+      <Navbar />
     </>
   );
 }
