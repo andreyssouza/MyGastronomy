2e329286930af3e98d28b7604e133e2473cf2110
Andrey Souza <andreyschowza@gmail.com>
2026-06-19 18:45:40 -0300

    Criando o popup dos plates com react

--- frontend/src/pages/plates/page.jsx
+++ frontend/src/pages/plates/page.jsx
@@ -1,16 +1,28 @@
 import plateServices from "../../services/plates";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from "../loading/page";
 import PlateCard from "../../components/platesCard/platesCard";
+import styles from "./page.module.css";
+import plateSelected from "../../components/platePopup/platePopup";
+import PlatePopup from "../../components/platePopup/platePopup";
 
 export default function Plates() {
   const { getAvailablesPlates, platesLoading, platesList, refetchPlates } = plateServices();
+  const [plateSelected, setPlateSelected] = useState(null);
 
   useEffect(() => {
     if (refetchPlates) {
       getAvailablesPlates();
     }
   }, [refetchPlates]);
+
+  const handlePlateSelected = (plate) => {
+    setPlateSelected(plate);
+  };
+
+  const handleClosePopup = () => {
+    setPlateSelected(null);
+  };
 
   if (platesLoading) {
     return <Loading />;
@@ -22,9 +34,19 @@ export default function Plates() {
     <>
       <div>
         {platesList.map((plate) => (
-          <PlateCard plateData={plate} key={plate._id} />
+          <div
+            key={plate._id}
+            className={styles.cardContainer}
+            onClick={() => {
+              handlePlateSelected(plate);
+            }}
+          >
+            <PlateCard plateData={plate} />
+          </div>
         ))}
       </div>
+
+      <div>{plateSelected && <PlatePopup plateData={plateSelected} onClose={handleClosePopup} />}</div>
     </>
   );
 }
